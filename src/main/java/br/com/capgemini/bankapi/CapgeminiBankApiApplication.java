221367ad38a1ece6d21e723bf31e57a8e0928e6e
Gustavo Pinheiro <gustavo.eng18@gmail.com>
2021-05-18 13:09:03 -0400

    first commit

---
+++ src/main/java/br/com/capgemini/bankapi/CapgeminiBankApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.capgemini.bankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgeminiBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapgeminiBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/capgemini/bankapi/CapgeminiBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.capgemini.bankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapgeminiBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
